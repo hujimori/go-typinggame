f29c84763cbb93bd7fbfd6c0f886d057b13d82bf
hujimori <hujimori16@gmail.com>
2022-02-19 22:24:13 +0900

    問題文を表示するように修正

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
 	"time"
 )
@@ -21,18 +22,49 @@ func input(r io.Reader) <-chan string {
 	return ch
 }
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var rs1Lettesr = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = rs1Lettesr[rand.Intn(len(rs1Lettesr))]
+	}
+
+	return string(b)
+}
+
+func askQuestion() string {
+	return randString(3)
+}
+
 func main() {
 	bc := context.Background()
-	t := 10 * time.Second
+	t := 20 * time.Second
 	ctx, cancel := context.WithTimeout(bc, t)
 	ch := input(os.Stdin)
 	defer cancel()
-
+	correctAns := 0
 	for {
+		q := askQuestion()
+		fmt.Println("Q.", q)
 		select {
 		case v := <-ch:
 			fmt.Println(">", v)
+			if v == q {
+				correctAns++
+				fmt.Println("correct!")
+			} else {
+				fmt.Println("incorrect!")
+			}
+
 		case <-ctx.Done():
+			// 入力途中で終了した場合のために改行する
+			fmt.Println()
+			fmt.Println("TotalCorrect:", correctAns)
 			fmt.Println("Time UP!")
 			return
 		}
